de4b86687377ff14f14e902ddf0b9b7cf1912f95
Jerry-Wemisiko <jerrywemisiko@gmail.com>
2025-04-11 16:16:25 +0300

    create  funcn to save qr code image to a DIRECTORY

---
+++ utils.py
@@ -0,0 +1,10 @@
+from pathlib import Path
+def save_qr_code_image(image, filename, output_dir="output"):
+    """
+    funcn to save qr code image to a DIRECTORY
+    output_dir="output" creates a directory named output
+    """
+    Path(output_dir).mkdir(parents=True, exist_ok=True)
+    full_path = f"{output_dir}/{filename}".png
+    image.save(full_path)
+    return
